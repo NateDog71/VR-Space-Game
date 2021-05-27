b2231b0916629255e0e56d3961975a35e9652d3a
subparace <williamsisaac1209@gmail.com>
2021-05-27 12:30:59 +1000

    Shield recharging added

--- VRSpaceGame/Assets/Scripts/Isaac/Enemy.cs
+++ VRSpaceGame/Assets/Scripts/Isaac/Enemy.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // Total health of the enemy
     public int health;
 
-    public int shields;
+    // Shields health
+    public float shields;
+
+    // Timer before shields start recharging
+    public float shieldsTimer;
+
+    // Rate the shield recharges at per second
+    public float rechargeRate;
+
+    // Max health for shields
+    float shieldsMax;
+
+    // Maximum timer
+    float timerMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize shields and timer maximums
+        shieldsMax = shields;
+        timerMax = shieldsTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If shields are not at full health, decrease timer
+        if (shields < shieldsMax)
+        {
+            shieldsTimer -= Time.deltaTime;
+        }
+
+        // If timer is zero, recharge shields
+        if (shieldsTimer < 0)
+        {
+            shields += Time.deltaTime * rechargeRate;
+
+            if (shields >= shieldsMax)
+            {
+                // Reset timer to and shields to maximum
+                shieldsTimer = timerMax;
+                shields = shieldsMax;
+            }
+        }
         
+        //if (true)
+        //{
+        //    //do damge stuff
+        //    shieldsTimer = timerMax;
+        //}
     }
 }
