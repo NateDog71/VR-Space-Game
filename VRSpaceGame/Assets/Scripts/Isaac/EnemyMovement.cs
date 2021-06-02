d21cc654d37afb5556cd2d417d766d52f456ff83
Isaac Williams <williamsisaac1209@gmail.com>
2021-06-02 15:06:47 +1000

    Added enemy movement between points

---
+++ VRSpaceGame/Assets/Scripts/Isaac/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+
+    public GameObject track;
+
+    Transform pointA;
+
+    Transform pointB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointA = track.transform.GetChild(0);
+        pointB = track.transform.GetChild(1);
+        transform.position = pointA.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA.position, pointB.position, time);
+    }
+}
