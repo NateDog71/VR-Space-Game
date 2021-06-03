236b0fde51e085340e94e0089271512831c8f3da
Benjamin Chaves <benjaminchavescode@gmail.com>
2021-06-03 18:14:46 +1000

    Added a visual console and Oculus headset & controller functionality into sandbox Scene. Added controller interface with index button & vertical touchpad input functionality.

---
+++ VRSpaceGame/Assets/Scripts/Benjamin/OculusControllerInterface.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusControllerInterface : MonoBehaviour
+{
+    public bool m_LogInput;
+
+    public VisualConsoleHandler m_VisualConsoleHandler;
+
+    public bool m_IndexTriggerPressed { get; private set; }
+    public bool m_IndexTriggerPressedThisFrame { get; private set; }
+    public bool m_IndexTriggerReleasedThisFrame { get; private set; }
+
+    public float m_TouchpadVertical { get; private set; }
+
+    private void Start()
+    {
+        m_VisualConsoleHandler.LogComment("Oculus Controller Interface initialised.");
+    }
+
+    private void Update()
+    {
+        CacheUserInput();
+
+        if(m_LogInput)
+        {
+            LogInput();
+        }
+    }
+
+    private void CacheUserInput()
+    {
+        m_IndexTriggerPressed = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+        m_IndexTriggerPressedThisFrame = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+        m_IndexTriggerReleasedThisFrame = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
+
+        m_TouchpadVertical = Input.GetAxis("Oculus_GearVR_DpadX") * -1;
+    }
+
+    private void LogInput()
+    {
+        if(m_IndexTriggerPressed)
+        {
+            m_VisualConsoleHandler.LogComment("Index trigger pressed.");
+        }
+
+        if(m_IndexTriggerPressedThisFrame)
+        {
+            m_VisualConsoleHandler.LogComment("Index trigger pressed this frame.");
+        }
+
+        if(m_IndexTriggerReleasedThisFrame)
+        {
+            m_VisualConsoleHandler.LogComment("Index trigger released this frame.");
+        }
+
+        if(!Mathf.Approximately(m_TouchpadVertical, 0f))
+        {
+            m_VisualConsoleHandler.LogComment("Touchpad Vertical: " + m_TouchpadVertical);
+        }
+    }
+}
---
+++ VRSpaceGame/Assets/Scripts/Benjamin/VisualConsoleHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualConsoleHandler : MonoBehaviour
+{
+    public TMPro.TextMeshPro m_ConsoleText;
+
+    private string m_consoleString;
+
+    public void ToggleConsoleActive()
+    {
+        bool newActive = !m_ConsoleText.gameObject.activeSelf;
+
+        SetConsoleActive(newActive);
+    }
+
+    public void SetConsoleActive(bool newActive)
+    {
+        m_ConsoleText.gameObject.SetActive(newActive);
+    }
+
+    private void UpdateConsoleString()
+    {
+        m_ConsoleText.text = m_consoleString;
+    }
+
+    public void LogComment(string commentToLog)
+    {
+        m_consoleString += "\n" + commentToLog;
+
+        UpdateConsoleString();
+    }
+
+    public void LogComment(float commentToLog)
+    {
+        m_consoleString += "\n" + commentToLog.ToString();
+
+        UpdateConsoleString();
+    }
+}
