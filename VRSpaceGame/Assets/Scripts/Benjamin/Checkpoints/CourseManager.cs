7ac95a5220d395366b86e4550d65146c3efebd91
Benjamin Chaves <benjaminchavescode@gmail.com>
2021-06-15 21:01:45 +1000

    Added course countdown timer.

--- VRSpaceGame/Assets/Scripts/Benjamin/Checkpoints/CourseManager.cs
+++ VRSpaceGame/Assets/Scripts/Benjamin/Checkpoints/CourseManager.cs
@@ -6,16 +6,54 @@ namespace Course
 {
     public class CourseManager : MonoBehaviour
     {
+        [Min(1)]
+        public int m_CourseSeconds;
+        private float m_remainingCourseSeconds;
+
         public CheckpointsManager m_CheckpointsManager;
+        public PlayerShip.CountdownTimer m_CountdownTimer;
 
         private void Start()
         {
+            AssertInspectorInputs();
+
             InitialiseReferences();
+
+            m_remainingCourseSeconds = m_CourseSeconds;
+        }
+
+        private void AssertInspectorInputs()
+        {
+            Debug.Assert(m_CountdownTimer != null);
         }
 
         private void InitialiseReferences()
         {
             m_CheckpointsManager.Initialise(this);
+        }
+
+        private void Update()
+        {
+            if (m_remainingCourseSeconds > 0f)
+            {
+                IncrementCourseCountdown();
+            }   
+        }
+
+        private void IncrementCourseCountdown()
+        {
+            m_remainingCourseSeconds -= Time.deltaTime;
+
+            if(m_remainingCourseSeconds > 0f)
+            {
+                m_CountdownTimer.UpdateCountdownText(m_remainingCourseSeconds);
+            }
+            else
+            {
+                m_CountdownTimer.UpdateCountdownText(0f);
+
+                EndCourse();
+            }
         }
 
         public void EndCourse()
---
+++ VRSpaceGame/Assets/Scripts/Benjamin/Player Ship/Ship UI/CountdownTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerShip
+{
+    public class CountdownTimer : MonoBehaviour
+    {
+        private TMPro.TextMeshPro m_textComponent;
+
+        private void Start()
+        {
+            m_textComponent = GetComponent<TMPro.TextMeshPro>();
+            Debug.Assert(m_textComponent != null);
+        }
+
+        public void UpdateCountdownText(float newCountdownSeconds)
+        {
+            m_textComponent.text = ((int)newCountdownSeconds).ToString();
+        }
+    }
+}
