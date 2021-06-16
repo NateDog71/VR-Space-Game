c80f7e3f80593752653625cf998b411bd84ba773
Isaac Williams <williamsisaac1209@gmail.com>
2021-06-16 23:40:41 +1000

    Movement update

--- VRSpaceGame/Assets/Scripts/Isaac/EnemyMovement.cs
+++ VRSpaceGame/Assets/Scripts/Isaac/EnemyMovement.cs
@@ -9,25 +9,62 @@ public class EnemyMovement : MonoBehaviour
     // Object containing the nodes the Enemy moves between
     public GameObject track;
 
+    public Transform parentTransform;
     // First point on track
     Transform pointA;
 
     // Second point on track
     Transform pointB;
+
+    public float rotateSpeed;
+
+    Transform target;
+
+    Vector3 direction;
+
+    Quaternion lookRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         pointA = track.transform.GetChild(0);
         pointB = track.transform.GetChild(1);
-        transform.position = pointA.position; 
+        parentTransform.position = pointA.position;
+        target = pointB;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Determines how far the object should be occilated between points
-        float Occilation = Mathf.PingPong(Time.time * speed, 1);
-        // Updates position
-        transform.position = Vector3.Lerp(pointA.position, pointB.position, Occilation);
+
+        //Debug.Log(direction);
+
+        direction = target.position - parentTransform.position.normalized;
+
+        lookRotation = Quaternion.LookRotation(direction);
+
+        parentTransform.rotation = Quaternion.Slerp(parentTransform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
+
+        parentTransform.position += parentTransform.forward * speed * Time.deltaTime;
+
+        //  // Determines how far the object should be occilated between points
+        //  float Occilation = Mathf.PingPong(Time.time * speed, 1);
+        //  // Updates position
+        //  transform.position = Vector3.Lerp(pointA.position, pointB.position, Occilation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Point A")
+        {
+            target = pointB;
+        }
+        if (other.gameObject.tag == "Point B")
+        {
+            Debug.Log("target switched");
+            target = pointA;
+        }
     }
 }
